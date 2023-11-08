490c3a8c95ef0588706fb409caa26b0c00fa7f04
novosma <mnovoseltsev>
2023-11-08 11:24:03 +0100

    small refactoring

--- src/app/football.service.ts
+++ src/app/football.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { TeamStanding } from "./team-standing";
-import { TeamStandingsComponent } from "./team-standings/team-standings.component";
 import { GameResult } from "./game-result";
+import { fake_standing_response, fake_standings_response_1 } from "./fake_standing_response";
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +16,7 @@ export class FootballService {
     if ([39, 148].includes(leagueId)) {
       return of(this.toTeamStandings(fake_standing_response)); // TODO
     }
-    return of(testTeams2);
+    return of(fake_standings_response_1);
   }
 
   toTeamStandings(response: any): TeamStanding[] {
@@ -64,128 +63,5 @@ const countryLeagueMapping: {[index: string]:any}  =
     'italy': 135
   }
 
-const testTeams1: TeamStanding[] = [
-  {
-    id: 38,
-    name: 'aha',
-    logoUrl: 'hi',
-    games: 20,
-    wins: 10,
-    draws: 1,
-    losses: 2,
-    goalDifference: 1,
-    points: 1,
-  },
-  {
-    id: 27,
-    name: 'uhu',
-    logoUrl: 'bye',
-    games: 15,
-    wins: 10,
-    draws: 1,
-    losses: 2,
-    goalDifference: 1,
-    points: 1,
-  }
-]
 
-const testTeams2: TeamStanding[] = [
-  {
-    id: 28,
-    name: 'FR',
-    logoUrl: 'BR',
-    games: 20,
-    wins: 10,
-    draws: 1,
-    losses: 2,
-    goalDifference: 1,
-    points: 1,
-  },
-  {
-    id: 27,
-    name: 'EN',
-    logoUrl: 'GB',
-    games: 15,
-    wins: 10,
-    draws: 1,
-    losses: 2,
-    goalDifference: 1,
-    points: 1,
-  }
-]
-
-const fake_standing_response = {
-  "get": "standings",
-  "parameters": {
-  "league": "39",
-    "season": "2019"
-},
-  "errors": [],
-  "results": 1,
-  "paging": {
-  "current": 1,
-    "total": 1
-},
-  "response": [
-  {
-    "league": {
-      "id": 39,
-      "name": "Premier League",
-      "country": "England",
-      "logo": "https://media.api-sports.io/football/leagues/2.png",
-      "flag": "https://media.api-sports.io/flags/gb.svg",
-      "season": 2019,
-      "standings": [
-        [
-          {
-            "rank": 1,
-            "team": {
-              "id": 40,
-              "name": "Liverpool",
-              "logo": "https://media.api-sports.io/football/teams/40.png"
-            },
-            "points": 70,
-            "goalsDiff": 41,
-            "group": "Premier League",
-            "form": "WWWWW",
-            "status": "same",
-            "description": "Promotion - Champions League (Group Stage)",
-            "all": {
-              "played": 24,
-              "win": 23,
-              "draw": 1,
-              "lose": 0,
-              "goals": {
-                "for": 56,
-                "against": 15
-              }
-            },
-            "home": {
-              "played": 12,
-              "win": 12,
-              "draw": 0,
-              "lose": 0,
-              "goals": {
-                "for": 31,
-                "against": 9
-              }
-            },
-            "away": {
-              "played": 12,
-              "win": 11,
-              "draw": 1,
-              "lose": 0,
-              "goals": {
-                "for": 25,
-                "against": 6
-              }
-            },
-            "update": "2020-01-29T00:00:00+00:00"
-          }
-        ]
-      ]
-    }
-  }
-]
-}
 
