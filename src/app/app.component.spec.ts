f0b63c590ba79a0e499728cd42d9a9456c5a1bb5
novosma <mnovoseltsev>
2023-11-09 09:20:54 +0100

    h1 to h2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('FOOTBALL UPDATES');
+    expect(compiled.querySelector('h2')?.textContent).toContain('FOOTBALL UPDATES');
   });
 });
