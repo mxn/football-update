f9e4edff5bddb01952f5756bd60e4a7b652995c9
novosma <mnovoseltsev>
2023-11-13 07:12:36 +0100

    Title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'FootballCert'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FootballCert');
+    expect(app.title).toEqual('Football Updates');
   });
 
   it('should render title', () => {
